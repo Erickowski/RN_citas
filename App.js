8cf4af933db211dfca2bd3c81680a1891afc3c20
Erick Mata <daniel_perez_03081995@hotmail.com>
2023-06-22 23:00:32 -0600

    create titles and styles

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, SafeAreaView } from 'react-native';
+
+import styles from './styles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>Administrador de citas</Text>
+        <Text style={[styles.title, styles.boldTitle]}>Veterinaria</Text>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f3f4f6',
+    flex: 1, // toma todo el espacio
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: '#374151',
+    fontWeight: '600'
+  },
+  boldTitle: {
+    fontWeight: '900',
+    color: '#6d28d9'
+  }
+})
+
+export default styles;
