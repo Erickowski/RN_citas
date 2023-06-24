59fb243f440bc931bb7f46fe4731f38932a21035
Erick Mata <daniel_perez_03081995@hotmail.com>
2023-06-23 22:56:16 -0600

    create and show modal

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
-import { Text, SafeAreaView, Button, Pressable } from 'react-native';
+import { useState } from 'react'
+import { Text, SafeAreaView, Modal, Pressable } from 'react-native';
 
 import styles from './styles';
 
 export default function App() {
+  const [showModal, setModalShow] = useState(false);
+
   const handlePress = () => {
-    console.log('hiciste click')
+    setModalShow(true)
   };
 
   return (
     <SafeAreaView style={styles.container}>
         <Text style={styles.title}>Administrador de citas</Text>
         <Text style={[styles.title, styles.boldTitle]}>Veterinaria</Text>
+
         <Pressable onPress={handlePress} style={styles.btn.newAppointment}>
           <Text style={styles.btn.textNewAppointment}>Nueva cita</Text>
         </Pressable>
+
+        <Modal visible={showModal} animationType='slide'>
+          <Text>Modal</Text>
+        </Modal>
     </SafeAreaView>
   );
 }
